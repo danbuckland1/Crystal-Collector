63c5549ca2e869e51f797a6171f81836d9e3a951
danbuckland1 <buckd409@gmail.com>
2018-06-13 23:18:51 -0400

    Making headway with functionson click calls and random number generator.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,6 +2,7 @@ var wins = 0;
 var losses = 0;
 var score = 0;
 var randomNumber 
+var crystalNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 //Random number is diplayed at start of game that is between 18-120
 var randomNumber = Math.floor(Math.random() * 120 + 1);
 document.getElementById('random').innerHTML;
@@ -9,25 +10,40 @@ document.getElementById('random').innerHTML;
 
 console.log(randomNumber);
 
+$("#random").text(randomNumber);
 
 //crystals have an assigned random number between 1-12
+var crystalNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+for(i = 1; i <= crystalNumbers.length; i++){
+    console.log([i]);
+var crystalPic = $("<img>");
+
+crystalPic.addClass("gem");
+
+crystalPic.attr("src", "assets/images/gem1.png");
+crystalPic.attr("src", "assets/images/gem2.png");
+crystalPic.attr("src", "assets/images/gem3.png");
+crystalPic.attr("src", "assets/images/gem4.png");
+
+crystalPic.attr("data-crystalvalue", crystalNumbers[i]);
 
 //user clicks crystal
-$(".gem1").on("click", function(){
+$(".gem").on("click", function(){
+    var crystalValue = ($(this).attr("data-crystalvalue"));
     alert("hi");
+
+    crystalValue = parseInt(crystalValue);
+
+    wins += crystalValue;
+    
+
 })
 
-$(".gem2").on("click", function(){
-    alert("you did it");
-})
 
-$(".gem3").on("click", function(){
-    alert("proud of you");
-})
 
-$(".gem4").on("click", function(){
-    alert("good job");
-})
+}
+
+
 //That number is then added to score
 
 //When user's are clicks equal to the random number, win goes up one point with text alert on page
