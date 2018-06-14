3f44f67052cd8802eb14f11af0b87409574cc3fd
danbuckland1 <buckd409@gmail.com>
2018-06-13 20:24:38 -0400

    Focusing on writing some basic variables finally and just seeing if I properly am able to use click fucntion on my images.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,7 @@
-
-var randomNumber
+var wins = 0;
+var losses = 0;
+var score = 0;
+var randomNumber 
 //Random number is diplayed at start of game that is between 18-120
 var randomNumber = Math.floor(Math.random() * 120 + 1);
 document.getElementById('random').innerHTML;
@@ -11,7 +13,21 @@ console.log(randomNumber);
 //crystals have an assigned random number between 1-12
 
 //user clicks crystal
+$(".gem1").on("click", function(){
+    alert("hi");
+})
 
+$(".gem2").on("click", function(){
+    alert("you did it");
+})
+
+$(".gem3").on("click", function(){
+    alert("proud of you");
+})
+
+$(".gem4").on("click", function(){
+    alert("good job");
+})
 //That number is then added to score
 
 //When user's are clicks equal to the random number, win goes up one point with text alert on page
