a60dbb3b569a56407cd5bea8fd8540046a38e12e
danbuckland1 <buckd409@gmail.com>
2018-06-14 22:05:57 -0400

    Last efforts of adding function to homework before handing it in.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,7 @@
 var wins = 0;
 var losses = 0;
-var score = 0;
+var score = [];
+var sum = 0;
 var randomNumber 
 var crystalNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 //Random number is diplayed at start of game that is between 18-120
@@ -8,40 +9,117 @@ var randomNumber = Math.floor(Math.random() * 120 + 1);
 document.getElementById('random').innerHTML;
 
 
-console.log(randomNumber);
+console.log("randomNumber:" + randomNumber);
 
 $("#random").text(randomNumber);
 
 //crystals have an assigned random number between 1-12
-var crystalNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-for(i = 1; i <= crystalNumbers.length; i++){
-    console.log([i]);
-var crystalPic = $("<img>");
+var crystalNumbers = Math.floor(Math.random() * 12 + 1);
+// for(i = 0; i <= crystalNumbers.length; i++){
 
-crystalPic.addClass("gem");
+    // console.log("crystalNumbers:" + crystalNumbers);
+// }
 
-crystalPic.attr("src", "assets/images/gem1.png");
-crystalPic.attr("src", "assets/images/gem2.png");
-crystalPic.attr("src", "assets/images/gem3.png");
-crystalPic.attr("src", "assets/images/gem4.png");
+//
+var blue = Math.floor(Math.random() * 12 + 1);
+console.log("blue" + blue);
 
-crystalPic.attr("data-crystalvalue", crystalNumbers[i]);
+var green = Math.floor(Math.random() * 12 + 1);
+console.log("green" + green);
+
+var red = Math.floor(Math.random() * 12 + 1);
+console.log("red" + red);
+
+var purple = Math.floor(Math.random() * 12 + 1);
+console.log("purple" + purple);
+
+
+// for (var i = 0; i < score.length; i++) {
+//     sum += score[i]
+
+//     console.log(sum);
+//   }
+
+
+
+
+$("#blueGem").on("click", function(){
+    alert("Blue gem clicked!"); 
+    score.push(blue);
+    sum = sum + blue;
+    console.log("sum" + sum);
+
+    console.log("score" + score);
+});
+$("#greenGem").on("click", function(){
+    alert("Green gem clicked!");
+    score.push(green);
+    sum = sum + green;
+    console.log("sum" + sum);
+
+    console.log("score" + score);
+ 
+});
+$("#redGem").on("click", function(){
+    alert("Red gem clicked!");
+    score.push(red);
+    sum = sum + red;
+    console.log("sum" + sum);
+
+    console.log("score" + score);   
+});
+$("#purpleGem").on("click", function(){
+    alert("Purple gem clicked!");  
+    score.push(purple);
+    sum = sum + purple;
+    console.log("sum" + sum);
+
+    console.log("score" + score); 
+});
+
+//Print sum to total score on each click
+
+
+//If total score equals sum of random number chosen win goes up
+
+//If total score is greater than random number loss goes up
+
+//Use start game function to reset when player wins or loses, values of gems and score
+
+
+
+
+
+
+
+
+
+// var crystalPic = $("<img>");
+
+// crystalPic.addClass("gem");
+
+// crystalPic.attr("src", "assets/images/gem1.png");
+// crystalPic.attr("src", "assets/images/gem2.png");
+// crystalPic.attr("src", "assets/images/gem3.png");
+// crystalPic.attr("src", "assets/images/gem4.png");
+
+// crystalPic.attr("data-crystalvalue", crystalNumbers[i]);
 
 //user clicks crystal
-$(".gem").on("click", function(){
-    var crystalValue = ($(this).attr("data-crystalvalue"));
-    alert("hi");
+// $(".gem").on("click", function(){
+//     var crystalValue = $(this).data("crystalvalue");
+//     alert(crystalValue);
 
-    crystalValue = parseInt(crystalValue);
+//     crystalValue = parseInt(crystalValue);
 
-    wins += crystalValue;
+//     wins += crystalValue;
     
 
-})
+// })
 
 
 
-}
+// }
 
 
 //That number is then added to score
